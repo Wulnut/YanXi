9c250aece870b4b979e81796a0f662a796aa701b
Wulnut <985951964@qq.com>
2021-01-26 15:24:59 +0800

    login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,22 +6,23 @@
           <form class="sign-in-form">
             <p class="title">登 录</p>
             <!-- element-ui 使用 -->
-            <el-form ref="loginFormRef"  label-width="0px" class="login_form">
+            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
               <!-- 用户名 -->
               <el-form-item prop="username">
                 <!-- v-model双向绑定 -->
-                <el-input prefix-icon="el-icon-search"
+                <el-input v-model="loginForm.username" prefix-icon="el-icon-search"
                           placeholder="支持手机号/邮箱登录"></el-input>
               </el-form-item>
               <!-- 密码 -->
               <el-form-item prop="password" >
-                <el-input  prefix-icon="iconfont icon-3702mima" type="password"
+                <el-input  v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"
                           placeholder="账户密码"></el-input>
               </el-form-item>
               <!-- 按钮区域 -->
             </el-form>
             <!-- element-ui 使用 -->
-            <el-button type="primary" class="btn solid">登 录</el-button>
+            <el-button type="primary" class="btn solid" @click="login"
+                       v-loading.fullscreen.lock="fullscreenLoading">登 录</el-button>
           </form>
           <form class="sign-up-form">
             <p class="title">注册</p>
@@ -154,6 +155,26 @@ export default {
       }
     };
     return {
+      // 登录表单数据绑定
+      loginForm: {
+        username: 'lyz',
+        password: '123'
+      },
+      // 表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          {required: true, message: '请输入手机号/邮箱', trigger: 'blur'},
+          {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+        ],
+        // 验证密码是否合法
+        password: [
+          {required: true, message: '请输入登录密码', trigger: 'blur'},
+          {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+        ]
+      },
+      fullscreenLoading: false,
+      // 注册
       ruleForm2: {
         pass: "",
         checkPass: "",
@@ -229,6 +250,37 @@ export default {
           return false;
         }
       })
+    },
+    // 登录
+    login() {
+      // 通过使用匿名函数，如果只有一个形参可以省略小括号
+      // 通过await & async来简化promise操作
+      this.$refs.loginFormRef.validate(async (valid) => {
+        console.log(valid);
+        if (!valid) return 0;
+        // await只能用于被async修饰的方法中,需要把该匿名函数修饰成异步的async方法
+        // 由于返回值中只有data有用，就解构其中的数据获取自己需要的即可
+        // post返回值是promise
+        // js对象装json JSON.stringify(对象)
+        const {data: result} = await this.$http.post('login', JSON.stringify(this.loginForm));
+        console.log(result);
+        // 判断状态码200为成功,400为失败
+        if (result.meta.status !== 200) {
+          return this.$message.error('登录失败');
+        } else {
+          // localStorage和sessionStorage区别，localStorage是持久化的存储
+          // 1.将登录成功之后的token，保存到客户端的sessionStorage中
+          //   1.1项目中除了登录之外的其他API接口，必须在登录之后才能访问
+          //   1.2token只应在当前网站访问打开期间生效，所以将token保存在sessionStorage中
+          // 2.通过编程式导航跳转到后台主页，路由地址是/home
+          this.fullscreenLoading = true;
+          setTimeout(() => {
+            this.fullscreenLoading = false;
+            this.$message.success('登录成功');
+            this.$router.push("/home");
+          }, 2000);
+        }
+      });
     },
     // <!--进入登录页-->
     gotoLogin() {
